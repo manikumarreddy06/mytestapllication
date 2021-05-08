8acf8e7e6d40bf8480c52f8be1b3534f65d37795
Your Name <you@example.com>
2021-05-08 12:26:31 +0530

    new features like button gradient added

--- app/src/main/java/com/myproject/myapplication/MainActivity.kt
+++ app/src/main/java/com/myproject/myapplication/MainActivity.kt
@@ -1,22 +1,26 @@
 package com.myproject.myapplication
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.myproject.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-  private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnSubmit.setOnClickListener {
-            Intent(this,homepageActivity::class.java).also {
+        binding.btnsubmit.setOnClickListener {
+            Intent(this, homepageActivity::class.java).also {
                 startActivity(it)
+
             }
         }
 
-        }
     }
+}
+
+
