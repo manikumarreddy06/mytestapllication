f4e4cea01a18f52977e4f7b4fff9b5109415e607
Your Name <you@example.com>
2021-06-18 16:15:37 +0530

    changes added

---
+++ app/src/main/java/com/myproject/myapplication/ProfitdetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.myproject.myapplication
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class ProfitdetailsActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+}
