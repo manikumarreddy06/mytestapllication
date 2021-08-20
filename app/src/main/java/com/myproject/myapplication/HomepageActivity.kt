2bdd3426ef4d77ee09838653f217a298202fa379
Your Name <you@example.com>
2021-08-20 12:45:15 +0530

    changes

--- app/src/main/java/com/myproject/myapplication/HomepageActivity.kt
+++ app/src/main/java/com/myproject/myapplication/HomepageActivity.kt
@@ -8,7 +8,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.myproject.myapplication.databinding.ActivityHomepageBinding
-
 import com.myproject.myapplication.network.PreferenceManager
 
 
